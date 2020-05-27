628ed460c5c8d199fa1e45ba100da5ff7b0b942e
Erick Flores <thearchitect@Flow-Station.local>
2020-05-27 02:06:34 -0500

    Need to fix routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ServiceOptionComponent } from './service-option/service-option.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
