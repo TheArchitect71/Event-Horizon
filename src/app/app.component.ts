4b0bdac36f0b3612fdb07adebbf839f9b08c7365
Erick Flores <thearchitect@Flow-Station.local>
2020-06-03 23:00:41 -0500

    service added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component} from '@angular/core';
+import { ClientsService } from './clients.service';
+import { Observable } from 'rxjs';
+import { Astronaut, FilterState, Filter, Option } from './types';
+import { MatDialog } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +11,18 @@ import { Component} from '@angular/core';
 })
 export class AppComponent {
   showFiller = false;
+  astronauts: Observable<Astronaut[]>;
+  filterState: FilterState;
+  filters: Observable<Filter[]>;
+
+  constructor(clientService: ClientsService, private dialog: MatDialog) {
+    this.astronauts = clientService.astronauts;
+    this.filterState = clientService.filterState;
+    this.filters = clientService.filters;
+  }
+
+  changeFilter(category: string, option: Option) {
+    this.filterState[category] = option;
+  }
+
 }
